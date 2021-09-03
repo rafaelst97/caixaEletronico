4dad72936eb3e1c625d7a0de93af089c1fb87038
Rafael dos Santos <rafaels1997@LIVE.COM>
2021-09-02 21:00:25 -0300

    Retirado enunciado das anotações

--- index.js
+++ index.js
@@ -36,23 +36,3 @@ function withdraw (value){
 }
 
 withdraw(85461);
-
-
-// Desenvolva um programa que simule a entrega de notas quando um cliente efetuar um saque em um caixa eletrônico. Os requisitos básicos são os seguintes:
-// •	Entregar o menor número de notas;
-// •	É possível sacar o valor solicitado com as notas de $1, $5, $10, $50 e $100;
-// •	Não se preocupe com o saldo. Ele é infinito.
-// •	Não se preocupe com a quantidade de cada nota no caixa eletrônico. Elas também são infinitas.
-// •	Os saques são sempre inteiros. Não se preocupe com casas decimais.
-// •	A saída pode ser simples ou gráfica. Isso será de sua escolha.
-// 
-// Exemplos:
-// 
-// Valor do saque: $30
-// Resultado esperado: 3 notas de $10.
-// 
-// Valor do saque: $75
-// Resultado esperado: 1 nota de $50. 2 notas de $10. 1 nota de $5.
-// 
-// Valor do saque: $163
-// Resultado esperado: 1 nota de $100. 1 nota de $50. 1 nota de $10. 3 notas de $1.
