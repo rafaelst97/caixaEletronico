19011295420945b3f4273bd9a579be32e3367691
Rafael dos Santos <rafaels1997@LIVE.COM>
2021-09-08 20:33:45 -0300

    Remoção do botão após enviar os valores

--- index.js
+++ index.js
@@ -15,39 +15,42 @@ function counter(value, bankNote){
 
 //Função de saque
 function toWithdraw (value){
-
-    let bankNotes = [500, 200, 100, 50, 20, 10, 5, 1];
-    let bankNotesCounted = [0, 0, 0, 0, 0, 0, 0, 0];
+    
+    let bankNotes = [100, 50, 30, 20, 10, 5, 2, 1];
+    let bankNotesCounted = [];
+    for (let i = 0; i < bankNotesCounted.length; i++){
+        bankNotesCounted.push(0);
+    }
 
     for (let i = 0; i < bankNotes.length; i++){
         bankNotesCounted[i] = counter (value, bankNotes[i]);
         value = value - (bankNotesCounted[i] * bankNotes[i]);
     }
-    
+
     let valueOfBankNotes = document.querySelector("#valueOfBankNotes"); //Cabeça da tabela
     let numberOfBankNotes = document.querySelector("#numberOfBankNotes"); //Corpo da tabela
     let valueTr = document.createElement("tr"); // Criação da linha responsável por ficar os tipos de cédulas
     let countedTr = document.createElement("tr"); // Criação da linha responsável pelo contador de cada cédula
     
     for (let i = 0; i < bankNotesCounted.length; i++){
-        
+            
         let valueTh = document.createElement("th"); // Células do topo da planilha com os valores de cada cédula
         let countedTd = document.createElement("td"); // Células com o contador de cada cédula
-        valueTh.textContent = bankNotes[i]; // Passagem do valor de cada cédula para o topo da tabela
-        countedTd.textContent = bankNotesCounted[i]; //Passagem da contagem de notas para o corpo da tabela
+        valueTh.textContent = `Nota de ${bankNotes[i]}`; // Passagem do valor de cada cédula para o topo da tabela
+        countedTd.textContent = bankNotesCounted[i]; //Passagem da contagem de notas para o corpo da tabela       
         valueTr.appendChild(valueTh);
         countedTr.appendChild(countedTd)
         valueOfBankNotes.appendChild(valueTr);
         numberOfBankNotes.appendChild(countedTr);
-
     }
+    enter.remove("#enter");
+
 }
 
 //Início do programa coletando o valor digitado após o clique no botão Enter 
 var enter = document.querySelector("#enter");
 enter.addEventListener("click", function (event){
     event.preventDefault();
-
     var form = document.querySelector("#inputForm");
     toWithdraw(form.input.value);
 })
