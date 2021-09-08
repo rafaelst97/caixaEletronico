a8454e5d1cf224f45c6985b7153fd9c73514381c
rafaelst97 <42190573+rafaelst97@users.noreply.github.com>
2021-09-08 15:19:05 -0300

    Inserção de nota de 20

--- index.js
+++ index.js
@@ -16,32 +16,43 @@ function counter(value, bankNote){
 //Função de saque
 function toWithdraw (value){
 
-    let bankNote100 = counter(value, 100);
-    value = value - (bankNote100 * 100);
-    let bankNote50 = counter(value, 50);
-    value = value - (bankNote50 * 50);
-    let bankNote10 = counter(value, 10);
-    value = value - (bankNote10 * 10);
-    let bankNote5 = counter(value, 5);
-    value = value - (bankNote5 * 5);
-    let bankNote1 = counter(value, 1);
-    value = value - (bankNote1 * 1);
+    let bankNotes = [100, 50, 20, 10, 5, 1];
+    let bankNotesCounted = [0, 0, 0, 0, 0, 0];
+
+    for (let i = 0; i < bankNotes.length; i++){
+        bankNotesCounted[i] = counter (value, bankNotes[i]);
+        value = value - (bankNotesCounted[i] * bankNotes[i]);
+    }
+
+    for (let i = 0; i < bankNotesCounted.length; i++){
+        console.log(`Cédula de: ${bankNotes[i]} : ${bankNotesCounted[i]}`)
+    }
+
+    for (let i = 0; i < bankNotesCounted.length; i++){
+        
+        let printNote = document.querySelector(`.bankNotes${bankNotes[i]}`);
+        printNote.textContent = bankNotesCounted[i];
+
+    }
 
     //Colocando o valor calculado no HTML
-    let bankNotes = document.querySelector(".bankNotes100");
-    bankNotes.textContent = bankNote100;
-
-    bankNotes = document.querySelector(".bankNotes50");
-    bankNotes.textContent = bankNote50;
-
-    bankNotes = document.querySelector(".bankNotes10");
-    bankNotes.textContent = bankNote10;
-
-    bankNotes = document.querySelector(".bankNotes5");
-    bankNotes.textContent = bankNote5;
-
-    bankNotes = document.querySelector(".bankNotes1");
-    bankNotes.textContent = bankNote1;
+    //let bankNotes = document.querySelector(".bankNotes100");
+    //bankNotes.textContent = bankNotes[1];
+    //
+    //bankNotes = document.querySelector(".bankNotes50");
+    //bankNotes.textContent = bankNotes[2];
+//
+    //bankNotes = document.querySelector(".bankNotes5");
+    //bankNotes.textContent = bankNote[5];
+//
+    //bankNotes = document.querySelector(".bankNotes10");
+    //bankNotes.textContent = bankNotes[3];
+//
+    //bankNotes = document.querySelector(".bankNotes5");
+    //bankNotes.textContent = bankNote5;
+//
+    //bankNotes = document.querySelector(".bankNotes1");
+    //bankNotes.textContent = bankNote1;
 }
 
 //Início do programa coletando o valor digitado após o clique no botão Enter 
