15679487a2e87198dba334853b70ae08a625d7b1
Rafael dos Santos <rafaels1997@LIVE.COM>
2021-09-08 18:40:04 -0300

    Montagem dinâmica realizada

--- index.js
+++ index.js
@@ -16,43 +16,31 @@ function counter(value, bankNote){
 //Função de saque
 function toWithdraw (value){
 
-    let bankNotes = [100, 50, 20, 10, 5, 1];
-    let bankNotesCounted = [0, 0, 0, 0, 0, 0];
+    let bankNotes = [500, 200, 100, 50, 20, 10, 5, 1];
+    let bankNotesCounted = [0, 0, 0, 0, 0, 0, 0, 0];
 
     for (let i = 0; i < bankNotes.length; i++){
         bankNotesCounted[i] = counter (value, bankNotes[i]);
         value = value - (bankNotesCounted[i] * bankNotes[i]);
     }
-
-    for (let i = 0; i < bankNotesCounted.length; i++){
-        console.log(`Cédula de: ${bankNotes[i]} : ${bankNotesCounted[i]}`)
-    }
-
+    
+    let valueOfBankNotes = document.querySelector("#valueOfBankNotes"); //Cabeça da tabela
+    let numberOfBankNotes = document.querySelector("#numberOfBankNotes"); //Corpo da tabela
+    let valueTr = document.createElement("tr"); // Criação da linha responsável por ficar os tipos de cédulas
+    let countedTr = document.createElement("tr"); // Criação da linha responsável pelo contador de cada cédula
+    
     for (let i = 0; i < bankNotesCounted.length; i++){
         
-        let printNote = document.querySelector(`.bankNotes${bankNotes[i]}`);
-        printNote.textContent = bankNotesCounted[i];
+        let valueTh = document.createElement("th"); // Células do topo da planilha com os valores de cada cédula
+        let countedTd = document.createElement("td"); // Células com o contador de cada cédula
+        valueTh.textContent = bankNotes[i]; // Passagem do valor de cada cédula para o topo da tabela
+        countedTd.textContent = bankNotesCounted[i]; //Passagem da contagem de notas para o corpo da tabela
+        valueTr.appendChild(valueTh);
+        countedTr.appendChild(countedTd)
+        valueOfBankNotes.appendChild(valueTr);
+        numberOfBankNotes.appendChild(countedTr);
 
     }
-
-    //Colocando o valor calculado no HTML
-    //let bankNotes = document.querySelector(".bankNotes100");
-    //bankNotes.textContent = bankNotes[1];
-    //
-    //bankNotes = document.querySelector(".bankNotes50");
-    //bankNotes.textContent = bankNotes[2];
-//
-    //bankNotes = document.querySelector(".bankNotes5");
-    //bankNotes.textContent = bankNote[5];
-//
-    //bankNotes = document.querySelector(".bankNotes10");
-    //bankNotes.textContent = bankNotes[3];
-//
-    //bankNotes = document.querySelector(".bankNotes5");
-    //bankNotes.textContent = bankNote5;
-//
-    //bankNotes = document.querySelector(".bankNotes1");
-    //bankNotes.textContent = bankNote1;
 }
 
 //Início do programa coletando o valor digitado após o clique no botão Enter 
